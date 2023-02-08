b44d6dacf0b36838bb5e29bef04c702626123d3c
zlatovvd <31865609+zlatovvd@users.noreply.github.com>
2023-02-08 22:51:14 +0200

    upd 08.02

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+let gallery = document.querySelector('.gallery');
+
+let markup = galleryItems
+    .map((item) =>
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img class="gallery__image" 
+                src="${item.preview}" 
+                alt = "${item.description}" 
+                data-source = "${item.original}"
+               />
+            </a>
+        </div>`
+    ).join("");
+    
+gallery.innerHTML = markup;
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const playerCurrentTime = localStorage.getItem("videoplayer-current-time");
+
+console.log('time start', localStorage.getItem("videoplayer-current-time"));
+
+if (playerCurrentTime) {
+    player.setCurrentTime(playerCurrentTime);
+}
+
+let setCurrentTime = throttle((currentTime) => {
+    console.log('player time', currentTime);
+    localStorage.setItem("videoplayer-current-time", currentTime);
+}, 1000);
+
+player.on('timeupdate', function (data) {
+    setCurrentTime(data.seconds);
+});
